b50efbaa2cdb5c7961adc91899516fa2e7186ddd
nick-parker <github@nicholasparker.org>
2013-09-21 21:05:31 -0700

    Committing so #python can see
    Just some changes to boundingBox.py

--- Basic 3d elements/boundingBox.py
+++ Basic 3d elements/boundingBox.py
@@ -1,7 +1,8 @@
 from Basics import *
 
+#Completed
 def box(mins,maxes):
-    #defines a box by its minimum and maximum values.
+    #defines a box mesh by its minimum and maximum values.
     a=sp.array([mins[0],mins[1],mins[2]])
     b=sp.array([mins[0],maxes[1],mins[2]])
     c=sp.array([maxes[0],maxes[1],mins[2]])
@@ -31,8 +32,9 @@ def box(mins,maxes):
         tri([a,e,f])])
 
 class boxRegion:
-    #A bounding box object meant to nest within itself to quickly define large regions as inside or outside a model. 
-    def __init__(self,xMax,yMax,zMax,xMin,yMin,zMin,depth=0):
+    #A bounding box object meant to nest within itself to quickly define large regions as inside or outside a mesh. 
+    def __init__(self,mesh,xMin,yMin,zMin,xMax,yMax,zMax,depth=0,maxDepth=1):
+        self.mesh = mesh #Stores the mesh which this box is used to study.
         self.xMax=float(xMax)
         self.xMin=float(xMin)
         self.yMax=float(yMax)
@@ -42,18 +44,21 @@ class boxRegion:
         self.xLen = abs(self.xMax-self.xMin)
         self.yLen = abs(self.yMax-self.yMin)
         self.zLen = abs(self.zMax-self.zMin)
-        self.mesh = box([xMin,yMin,zMin],[xMax,yMax,zMax])
-        self.center = sp.array([(self.xMax+self.xMin)/2,(self.Ymax+self.yMin)/2,(self.zMax+self.Zmin)/2])
-        self.edges = []
-        self.solid = True #This flag states whether the region is homogenous. 
-        self.inside = False #This flag states whether the region is inside or outside a mesh.
+        self.box = box([xMin,yMin,zMin],[xMax,yMax,zMax]) #Mesh box around boxregion, to be used in checking edges.
+        self.center = sp.array([(self.xMax+self.xMin)/2,(self.yMax+self.yMin)/2,(self.zMax+self.zMin)/2])
+        self.edges = [] #This list contains all edges which can or do interact with 
+        self.solid = None #This flag states whether the region is homogenous. 
+        self.inside = None #This flag states whether the region is inside or outside a mesh.
         self.depth = depth #This counts how many boxRegions self is nested within.
-        self.sections = []
+        self.maxDepth = maxDepth
+        self.children = []
+    def __str__(self):
+        return str(self.xMin) + ' ' + str(self.yMin) + ' ' + str(self.zMin) + ' ' + str(self.xMax) + ' ' + str(self.yMax) + ' ' + str(self.zMax)
     def contains(self,p):
-        #Checks if point p is within self. This does not change self.solid, self.inside or self.sections.
+        #Checks if point p is within self. This does not change self.solid, self.inside or self.children. Completed.
         return self.xMin<=p[0]<=self.xMax and self.yMin<=p[1]<=self.yMax and self.zMin<=p[2]<=self.zMax
     def checkPoints(self,m):
-        #Checks if mesh m has any points inside self. Sets self.solid to false if there are any.
+        #Checks if mesh m has any points inside self. Sets self.solid to false if there are any. Completed.
         for point in m.points:
             if self.contains(point):
                 self.solid=False
@@ -70,7 +75,7 @@ class boxRegion:
                 if self.contains(edge.a):
                     self.edges.append(edge)
                     self.solid=False
-                elif self.mesh.edge_intersect(e):
+                elif self.box.edge_intersect(e):
                     self.edges.append(edge)
                     self.solid=False
         else:
@@ -80,15 +85,31 @@ class boxRegion:
                 if self.contains(edge.a):
                     self.edges.append(edge)
                     self.solid=False
-                elif self.mesh.edge_intersect(e):
+                elif self.box.edge_intersect(e):
                     self.edges.append(edge)
                     self.solid=False
+    def checkInside(self,m):
+        #Returns self.inside unless self.solid is False, in which case it prints for now. Sophisticated error management comes later.
+        if not self.solid: print "Region not homogeneous, unclear if inside or outside."
+        elif self.inside != None: return self.inside
+        else:
+            return m.contains(self.senter)
+    def tesselate(self):
+        #Splits all nonsolid regions which do not yet have children. Effectively makes the tree one level deeper. Will not exceed maxdepth.
+        self.checkEdges(self.mesh)
+        if not self.solid:
+            if self.depth<self.maxDepth:
+                if self.children==[]:
+                    xMid = (self.xMax+self.xMin)/2
+                    yMid = (self.yMax+self.yMin)/2
+                    zMid = (self.zMax+self.zMin)/2
+                    childA = boxRegion(self.mesh, self.xMin, self.yMin, self.zMin, xMid, yMid, zMid, self.depth+1, self.maxDepth)
+                    childB = boxRegion(self.mesh, xMid, yMid, zMid, self.xMax, self.yMax, self.zMax, self.depth+1, self.maxDepth)
+                    self.children=[childA,childB]
+                else:
+                    for child in self.children:
+                        child.tesselate()
 
 
-
-#This file is incomplete but I'm pushing anyway before going to the gym. The plan is to just make mesh boxes for all the bounding boxes to perform checkEdges.
-
-#The overall plan is to write a function in slice.py which takes the mesh, draws a boxRegion over its extents, then subdivides that boxRegion until all subdivisions are either homogenous (self.solid = True) or at the max depth which
-#you include in the input to said function. Then whole areas of layers can just be declared solid or empty without having to do a bunch more checks on crazy complicated meshes. Also, I'm going to add a list of edges which interact with
-#a given boxRegion, since if an edge doesn't interact with a region, no child regions will interact with it either. Just need to work out how to recognize edges that are inside but don't cross in an efficient manner.
-        
+testMesh = box([0,0,0],[15,15,15])
+testBox = boxRegion(testMesh,0,0,0,20,20,20)
